b44c5a0ad287cc2cba7b5f10ad64cc2fb225c138
Jairo Duarte <elihro.duarte@gmail.com>
2017-08-29 17:41:37 -0500

    Actualizado a v0.2

--- pagina/views.py
+++ pagina/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def estadisticas(request):
+    return render(request, 'pagina/estadisticas.html', {})
+
+
 def index(request):
     return render(request, 'pagina/index.html', {})
+
+
+def reglas(request):
+    return render(request, 'pagina/reglas.html', {})
+
